54a33549e689132c6214e57ef8c29e0622bfe93c
ttheoto <31117479+ttheoto@users.noreply.github.com>
2020-05-18 10:28:39 +0200

    feat: Transfer Notebook to .py

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,17 +1,41 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath) 
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, how='left', on=('id'))
+    return df
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(";", expand=True)
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+    # use this row to extract a list of new column names for categories
+    category_colnames = list(row.str.split("-", expand = True)[0])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split("-").str[1]
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    # drop the original categories column from `df`
+    df = df.drop(columns=['categories'])
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # drop duplicates
+    df = df.drop_duplicates()
+    return df
 
-
-def save_data(df, database_filename):
-    pass  
-
+def save_data(df, database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df.to_sql('Messages', engine, index=False)  
 
 def main():
     if len(sys.argv) == 4:
