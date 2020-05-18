ee89a758ac29c1b83767f755c5b0a5328f1d02ed
ttheoto <31117479+ttheoto@users.noreply.github.com>
2020-05-18 21:33:23 +0200

    feat: Change Train Test Split

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -68,9 +68,7 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, 
-                                                            test_size=0.7,
-                                                            random_state=10)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.7)
         
         print('Building model...')
         model = build_model()
